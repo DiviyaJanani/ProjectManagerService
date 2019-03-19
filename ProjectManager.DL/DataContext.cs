d3c1194cbb4267ca681a5d6114e402f50124f091
Administrator <Administrator@DOTNET>
2019-03-19 02:55:24 -0700

    ProjectManagerService test

--- ProjectManager.DL/DataContext.cs
+++ ProjectManager.DL/DataContext.cs
@@ -10,7 +10,7 @@ namespace ProjectManager.DL
 {
     public class DataContext : DbContext
     {
-        //connectionString
+        
         public DataContext() : base("name = ProjectManagerConnectionString")
         {
         }
